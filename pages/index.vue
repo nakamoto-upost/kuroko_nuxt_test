8e81b79d96ea63ed6aeaf30da68656eda66c4502
nakamoto-upost <nakamoto.t@u-post.co.jp>
2022-12-07 02:29:27 +0000

    third commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>{{response.details.subject}}</h1>
+    <div>{{response.details.ext_2}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    return { response: await $axios.$get('/rcms-api/3/service/3') };
+  },
+};
+</script>
